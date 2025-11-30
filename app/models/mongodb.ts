2d26c2d0341baef775ec2f26f4aed380a8b09992
ESHWAR1024 <eshwar10245@gmail.com>
2025-11-30 13:26:52 +0530

    some changes

--- app/models/mongodb.ts
+++ app/models/mongodb.ts
@@ -1,4 +1,4 @@
-import { connectDB as _connectDB } from '@/lib/mongodb';
+import { connectDB as _connectDB } from '@/app/lib/mongodb';
 
 export const connectDB = _connectDB;
 export default _connectDB;
